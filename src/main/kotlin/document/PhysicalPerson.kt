e7504439f052b70a64bf3de368b49932dbe802c2
Arthur Marinheiro Vilela <av@westron.media>
2021-04-12 13:17:28 -0300

    Adiconado data de nascimento a classe PhysicalPerson e serilaizer para classe de LocalDate

--- src/main/kotlin/document/PhysicalPerson.kt
+++ src/main/kotlin/document/PhysicalPerson.kt
@@ -1,11 +1,15 @@
 package document
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import util.serializer.LocalDateSerializer
+import java.time.LocalDate
 
 /**
  * Representa uma pessoa física (indivíduo)
  *
  * @property cpf        cpf do indivíduo
+ * @property birthDay   data de nascimento do indivíduo
  * @property sex        sexo do indivíduo
  */
 @Serializable
@@ -13,6 +17,8 @@ class PhysicalPerson(
     override val id: String?,
     override val name: String,
     val cpf: String,
+    @Serializable(with = LocalDateSerializer::class)
+    val birthDay: LocalDate,
     val sex: Sex
 ):Person() {
 }
---
+++ src/main/kotlin/util/serializer/LocalDateSerializer.kt
@@ -0,0 +1,25 @@
+package util.serializer
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Serializer(forClass = LocalDate::class)
+class LocalDateSerializer :KSerializer<LocalDate> {
+    val formatter = DateTimeFormatter.ISO_DATE
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString(), formatter)
+    }
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.format(formatter))
+    }
+}
