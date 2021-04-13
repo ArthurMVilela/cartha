3baf9b58069f958c1465354f84e8d66a99c33847
Arthur Marinheiro Vilela <av@westron.media>
2021-04-12 21:26:55 -0300

    Refatorado Affiliation para associar filiação a uma pessoa física

--- src/main/kotlin/document/civilRegistry/Affiliation.kt
+++ src/main/kotlin/document/civilRegistry/Affiliation.kt
@@ -1,11 +1,13 @@
 package document.civilRegistry
 
+import document.PhysicalPerson
 import kotlinx.serialization.Serializable
 
 /**
  * Representa uma filiação
  *
  * @property id             Identificador único
+ * @property personId       ID da pessoa relacionada a esta filiação
  * @property name           Nome da pessoa a que se refere essa filiação
  * @property UF             Unidade federativa que a pessoa é natural
  * @property Municipality   Município que a pessoa é natural
@@ -13,8 +15,15 @@ import kotlinx.serialization.Serializable
 @Serializable
 class Affiliation(
     val id:String?,
+    val personId: String,
     val name:String,
     val UF:String?,
     val Municipality:String?
 ) {
+    constructor(
+        id:String?,
+        person:PhysicalPerson,
+        UF:String?,
+        Municipality:String?
+    ):this(id, person.id!!, person.name, UF, Municipality)
 }
