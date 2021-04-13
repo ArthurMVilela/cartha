bfecde630f0010cd1036570e32aca50c3d2826b5
Arthur Marinheiro Vilela <av@westron.media>
2021-04-13 19:26:28 -0300

    Implementado DAO para Grandparent

--- src/main/kotlin/document/persistency/dao/civilRegistry/GrandparentDAO.kt
+++ src/main/kotlin/document/persistency/dao/civilRegistry/GrandparentDAO.kt
@@ -2,34 +2,109 @@ package document.persistency.dao.civilRegistry
 
 import document.civilRegistry.Grandparent
 import document.persistency.dao.DAO
-import org.jetbrains.exposed.sql.Op
+import document.persistency.tables.civilRegistry.grandparentTable
+import org.jetbrains.exposed.exceptions.ExposedSQLException
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class GrandparentDAO:DAO<Grandparent,String> {
     override fun insert(obj: Grandparent) {
-        TODO("Not yet implemented")
+        try {
+            transaction {
+                grandparentTable.insert {
+                    it[id] = obj.id!!
+                    it[personId] = obj.personId
+                    it[name] = obj.name
+                    it[type] = obj.type
+                    it[UF] = obj.UF
+                    it[municipality] = obj.municipality
+                }
+            }
+        } catch (ex: ExposedSQLException) {
+            throw ex
+        }
     }
 
     override fun select(id: String): Grandparent? {
-        TODO("Not yet implemented")
+        var p: Grandparent? = null
+        try {
+            val results = selectMany(Op.build { grandparentTable.id eq id })
+            if (!results.isEmpty()) {
+                p = selectMany(Op.build { grandparentTable.id eq id }).first()
+            }
+            return p
+        } catch (ex: ExposedSQLException) {
+            throw ex
+        }
     }
 
     override fun selectMany(condition: Op<Boolean>): List<Grandparent> {
-        TODO("Not yet implemented")
+        var list:List<Grandparent> = listOf()
+        try {
+            transaction {
+                val found = grandparentTable.select {
+                    condition
+                }.map {
+                    Grandparent(
+                        it[grandparentTable.id],
+                        it[grandparentTable.personId],
+                        it[grandparentTable.name],
+                        it[grandparentTable.type],
+                        it[grandparentTable.UF],
+                        it[grandparentTable.municipality],
+                    )
+                }
+                list = found
+            }
+
+            return list
+        } catch (ex: ExposedSQLException) {
+            throw ex
+        }
     }
 
     override fun selectAll(): List<Grandparent> {
-        TODO("Not yet implemented")
+        var list:List<Grandparent> = listOf()
+        try {
+            list = selectMany(Op.build { grandparentTable.id eq grandparentTable.id })
+            return list
+        } catch (ex: ExposedSQLException) {
+            throw ex
+        }
     }
 
     override fun update(oldId: String, new: Grandparent) {
-        TODO("Not yet implemented")
+        try {
+            transaction {
+                grandparentTable.update({ grandparentTable.id eq oldId }) {
+                    it[id] = new.id!!
+                    it[personId] = new.personId
+                    it[name] = new.name
+                    it[type] = new.type
+                    it[UF] = new.UF
+                    it[municipality] = new.municipality
+                }
+            }
+        } catch (ex: ExposedSQLException) {
+            throw ex
+        }
     }
 
     override fun delete(id: String) {
-        TODO("Not yet implemented")
+        try {
+            deleteWhere(Op.build { grandparentTable.id eq id })
+        } catch (ex: ExposedSQLException) {
+            throw ex
+        }
     }
 
     override fun deleteWhere(condition: Op<Boolean>) {
-        TODO("Not yet implemented")
+        try {
+            transaction {
+                grandparentTable.deleteWhere { condition }
+            }
+        } catch (ex: ExposedSQLException) {
+            throw ex
+        }
     }
 }
