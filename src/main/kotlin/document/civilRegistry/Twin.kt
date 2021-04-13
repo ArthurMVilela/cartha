403fc18566641302f58f4724778894224917e288
Arthur Marinheiro Vilela <av@westron.media>
2021-04-12 21:35:51 -0300

    Refatorado Twin

--- src/main/kotlin/document/civilRegistry/Twin.kt
+++ src/main/kotlin/document/civilRegistry/Twin.kt
@@ -3,17 +3,22 @@ package document.civilRegistry
 import kotlinx.serialization.Serializable
 
 /**
- * Presenta o regitro de gemeos numa sertidão de nascimento
+ * Presenta o regitro de gemeos numa certidão de nascimento
  *
- * @property id             Identificador único
- * @property registration   Matrícula da certidão de nascimento do gemeo
- * @property name           Nome do gemeo
+ * @property id                     Identificador único
+ * @property birthCertificateId     ID da certidão de nascimento que se relaciona a essa classe
+ * @property registration           Matrícula da certidão de nascimento do gemeo
+ * @property name                   Nome do gemeo
  */
 @Serializable
 class Twin(
     val id:String?,
+    val birthCertificateId:String,
     val registration:String,
     val name:String
 ) {
-    constructor(id:String?, birthCertificate: BirthCertificate):this(id, birthCertificate.registration, birthCertificate.name)
+    constructor(
+        id:String?,
+        birthCertificate: BirthCertificate
+    ):this(id, birthCertificate.id!!, birthCertificate.registration, birthCertificate.name)
 }
