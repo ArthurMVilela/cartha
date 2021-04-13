a89a25518ffb42d8968497ae9db2eb92881049e9
Arthur Marinheiro Vilela <av@westron.media>
2021-04-12 21:33:35 -0300

    Refatorado Grandparent

--- src/main/kotlin/document/civilRegistry/Grandparent.kt
+++ src/main/kotlin/document/civilRegistry/Grandparent.kt
@@ -1,22 +1,32 @@
 package document.civilRegistry
 
+import document.PhysicalPerson
 import kotlinx.serialization.Serializable
 
 /**
- * Representa o registro de um avó/avô numa certidão de nascimento
+ * Representa o registro de um avó/avô numa certidão de nascimento.
  *
  * @property id             Identificador único
+ * @property personId       ID da pessoa que se relaciona essa classe
  * @property type           Tipo de avó/avo (Materno/Paterno)
  * @property name           Nome da pessoa a que se refere essa filiação
  * @property UF             Unidade federativa que a pessoa é natural
- * @property Municipality   Município que a pessoa é natural
+ * @property municipality   Município que a pessoa é natural
  */
 @Serializable
 class Grandparent(
     val id:String?,
-    val type:GrandparentType,
+    val personId:String,
     val name:String,
+    val type:GrandparentType,
     val UF:String?,
-    val Municipality:String?
+    val municipality:String?
 ) {
+    constructor(
+        id:String?,
+        person: PhysicalPerson,
+        type:GrandparentType,
+        UF:String?,
+        municipality:String?
+    ):this(id, person.id!!, person.name, type, UF, municipality)
 }
