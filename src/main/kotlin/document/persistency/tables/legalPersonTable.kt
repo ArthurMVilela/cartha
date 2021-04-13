64eeb238021ef8d6e92ac96c4e4d8da72b19b0fd
Arthur Marinheiro Vilela <av@westron.media>
2021-04-13 17:26:37 -0300

    Implementado DAO para classe LegalPerson

--- src/main/kotlin/document/persistency/tables/legalPersonTable.kt
+++ src/main/kotlin/document/persistency/tables/legalPersonTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Table
 
 object legalPersonTable : Table("legal_person") {
     val id = char("id", 32).references(personTable.id)
-    val cpnj = char("cnpj", 14)
+    val cnpj = char("cnpj", 14)
 
     override val primaryKey = PrimaryKey(id, name = "pk_legal_person_id")
 }
