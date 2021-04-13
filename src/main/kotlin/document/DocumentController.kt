92cfc3a5934b93be4297505781c3603fb4776353
Arthur Marinheiro Vilela <av@westron.media>
2021-04-13 14:50:04 -0300

    Conexão ao banco de dados e interface DAO

--- src/main/kotlin/document/DocumentController.kt
+++ src/main/kotlin/document/DocumentController.kt
@@ -1,8 +1,21 @@
 package document
 
+import document.persistency.tables.*
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
 /**
  * Classe controller para o pacode de documentos
  */
 class DocumentController {
-
+    init {
+        setupTables()
+    }
+    private fun setupTables() {
+        transaction {
+            SchemaUtils.create(
+                personTable, physicalPersonTable, legalPersonTable, officialTable
+            )
+        }
+    }
 }
--- src/main/kotlin/runDocumentService.kt
+++ src/main/kotlin/runDocumentService.kt
@@ -5,8 +5,18 @@ import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 fun main() {
+    val db = Database.connect(
+        url = "jdbc:mysql://localhost:3306/cartha_document_db",
+        driver = "com.mysql.jdbc.Driver",
+        user = "root",
+        password = "test"
+    )
+    TransactionManager.defaultDatabase = db
+
     val service = DocumentService()
     embeddedServer(Netty, port = 8080) {
         install(ContentNegotiation)  {
