a693d3121f84fabde9c1e4b6641b5547c77aaaf9
Arthur Marinheiro Vilela <av@westron.media>
2021-04-13 00:21:41 -0300

    Tabelas para DeathCertificate

--- src/main/kotlin/document/persistency/tables/DocumentTable.kt
+++ src/main/kotlin/document/persistency/tables/DocumentTable.kt
@@ -1,14 +1,15 @@
 package document.persistency.tables
 
-import document.DocumentStatus
-import document.Sex
-import document.UF
+import document.*
+import document.civilRegistry.Affiliation
 import document.civilRegistry.MatrimonialRegime
 import document.civilRegistry.Spouse
 import document.persistency.tables.BirthCertificateTable.references
+import document.persistency.tables.MarriageCertificateTable.references
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.`java-time`.date
 import org.jetbrains.exposed.sql.`java-time`.datetime
+import java.time.LocalDateTime
 
 object DocumentTable : Table("document") {
     val id = char("id", 44)
@@ -61,6 +62,27 @@ object MarriageCertificateTable : Table("marriage_certificate") {
     val matrimonialRegime = enumeration("matrimonial_regime", MatrimonialRegime::class)
 
     override val primaryKey = PrimaryKey(id, name = "pk_marriage_certificate_id")
+}
+
+object DeathCertificateTable : Table("death_certificate") {
+    val id = char("id", 44).references(CivilRegistryDocumentTable.id)
+    val sex = enumeration("sex", Sex::class)
+    val color = enumeration("color", Color::class)
+    val civilStatus = enumeration("civil_status", CivilStatus::class)
+    val age = integer("age")
+
+    val birthPlace = varchar("birth_place", 120)
+    val documentOfIdentity = varchar("document_of_identity", 20)
+    val affiliationId = char("id", 32).references(AffiliationTable.id)
+    val residency = varchar("residency", 120)
+
+    val dateTimeOfDeath = datetime("datetime_of_death")
+    val placeOfDeath = varchar("place_of_death", 80)
+    val causeOfDeath  = varchar("cause_of_death", 80)
+    val burialOrCremationLocation = varchar("burial_or_cremation_location", 120)
+    val documentDeclaringDeath = varchar("document_declaring_death", 80)
+
+    override val primaryKey = PrimaryKey(id, name = "pk_death_certificate_id")
 }
 
 object SpouseTable : Table("spouse") {
