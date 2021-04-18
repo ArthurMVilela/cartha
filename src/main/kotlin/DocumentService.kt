41c92c3b3c9a4d30fcba2e92bda00d775c978b34
Arthur Marinheiro Vilela <av@westron.media>
2021-04-18 10:24:48 -0300

    Adicionado configurações para containers de banco de dados para documentos e serviço de documentos

--- src/main/kotlin/DocumentService.kt
+++ src/main/kotlin/DocumentService.kt
@@ -14,13 +14,24 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 fun main() {
-    val db = Database.connect(
-        url = "jdbc:mysql://localhost:3306/cartha_document_db?verifyServerCertificate=false&useSSL=true",
-        driver = "com.mysql.jdbc.Driver",
-        user = "root",
-        password = "test"
-    )
-    TransactionManager.defaultDatabase = db
+    try {
+        val host = System.getenv("DATABASE_HOST")
+        val port = System.getenv("DATABASE_PORT")
+        val database = System.getenv("DATABASE_NAME")
+        val user = System.getenv("DATABASE_USER")
+        val password = System.getenv("DATABASE_PASSWORD")
+        val url = "jdbc:mysql://$host:$port/$database?verifyServerCertificate=false&useSSL=true"
+        val db = Database.connect(
+            url = url,
+            driver = "com.mysql.jdbc.Driver",
+            user = user,
+            password = password,
+        )
+
+        TransactionManager.defaultDatabase = db
+    } catch (e:Exception) {
+        println(e.message)
+    }
 
     val service = DocumentService()
     embeddedServer(Netty, port = 8080) {
