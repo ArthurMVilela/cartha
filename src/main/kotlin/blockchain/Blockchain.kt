2239e7f4d1aa0c4914278ffbbddd09b93b32eab5
Arthur Marinheiro Vilela <av@westron.media>
2021-04-26 17:24:07 -0300

    Adicionado novo método para adicionar bloco a blockchain e comentarios para documentação

--- src/main/kotlin/blockchain/Blockchain.kt
+++ src/main/kotlin/blockchain/Blockchain.kt
@@ -6,8 +6,13 @@ import java.time.Month
 import java.time.Year
 import java.util.*
 
+/**
+ *  Representa uma blockchain
+ *
+ *  @property blocks        Lista de blocos da blockchain, caso vázia, a blockchain cria um bloco genesys
+ */
 @Serializable
-class Blockchain(val blocks:MutableList<Block> = mutableListOf()) {
+class Blockchain(private val blocks:MutableList<Block> = mutableListOf()) {
     init {
         if (blocks.isEmpty()) {
             val genesys = createGenesys()
@@ -15,25 +20,59 @@ class Blockchain(val blocks:MutableList<Block> = mutableListOf()) {
         }
     }
 
+    /**
+     * Adiciona um bloco à blockchain
+     *
+     * @param block         bloco a ser adicionado
+     */
     fun addBlock(block: Block) {
+        if (block.transactions.size != Config.transactionAmount) {
+            println("error")
+        }
         blocks.add(block)
     }
 
+    /**
+     * Adiciona um bloco à blockchain
+     *
+     * @param timestamp             timestamp do bloco
+     * @param transactions          lista das transações do bloco
+     */
+    fun addBlock(timestamp: LocalDateTime, transactions: List<Transaction>) {
+        this.addBlock(Block(timestamp, transactions, getLast().hash!!))
+    }
+
+    /**
+     * Busca o último bloco da blockchain
+     *
+     * @return Último bloco da blockchain
+     */
     fun getLast():Block {
         return blocks.last()
     }
 
+    /**
+     * Busca um bloco da blockchain com uma id específica
+     *
+     * @param id            id do bloco
+     *
+     * @return bloco com a id especificada
+     */
     fun getBlock(id:String): Block {
         return blocks.first { it.id == id }
     }
 
+    /**
+     * Valida a blockchain
+     *
+     * @return se a blockchain é válida
+     */
     fun validateChain():Boolean {
         blocks.forEachIndexed { index, block ->
 
             if (block.hash != block.createHash()) return false
             if (index > 0) {
                 val previous = blocks[index - 1]
-                println(previous.hash != block.previousHash)
                 if (previous.hash != block.previousHash) return false
             } else {
                 if (createGenesys().timestamp != block.timestamp) return false
@@ -45,6 +84,11 @@ class Blockchain(val blocks:MutableList<Block> = mutableListOf()) {
         return true
     }
 
+    /**
+     * Cria o bloco genesys
+     *
+     * @return bloco genesys
+     */
     private fun createGenesys():Block {
         val timestamp = LocalDateTime.of(Year.MIN_VALUE, Month.JANUARY, 1, 0, 0,0,0)
         return Block(timestamp, listOf(), "")
