ee2cf91cbe6e6d2a005deb888eb795d8a2cee711
Arthur Marinheiro Vilela <av@westron.media>
2021-05-13 16:03:38 -0300

    Adicionado contrutor segundário para AccessLog

--- src/main/kotlin/authentication/logging/AccessLog.kt
+++ src/main/kotlin/authentication/logging/AccessLog.kt
@@ -1,5 +1,7 @@
 package authentication.logging
 
+import authentication.UserSession
+import authentication.exception.UserOfflineException
 import kotlinx.serialization.Serializable
 import util.serializer.LocalDateTimeSerializer
 import util.serializer.UUIDSerializer
@@ -8,6 +10,12 @@ import java.util.*
 
 /**
  * Representa um log de acesso
+ *
+ * @param id            identificador único do log
+ * @param sessionId     id da sessão em que a ação ocorreu
+ * @param userId        id do usuário que executou a ação
+ * @param timestamp     instante em que a ação ocorreu
+ * @param action        ação que se refere o log
  */
 @Serializable
 class AccessLog (
@@ -21,4 +29,31 @@ class AccessLog (
     val timestamp: LocalDateTime,
     val action: Action
 ){
+    constructor(
+        session: UserSession,
+        action: Action,
+        timestamp: LocalDateTime
+    ):this(
+        createId(),
+        session.id,
+        session.user.id,
+        timestamp,
+        action
+    ) {
+        if (session.hasEnded()) {
+            throw UserOfflineException("A sessão já foi terminada.")
+        }
+    }
+
+    companion object {
+        /**
+         * Cria o identificador único para este log de acesso
+         *
+         * @return UUID para a id do log de acesso
+         */
+        private fun createId():UUID {
+            return UUID.randomUUID()
+        }
+
+    }
 }
