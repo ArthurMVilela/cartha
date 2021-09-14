02defbcc44538fb22a905ca9ad6a1a9b6603f9b2
Arthur Marinheiro Vilela <av@westron.media>
2021-09-13 22:12:57 -0300

    Adicionado classes de certição de nascimento

---
+++ src/main/kotlin/newDocument/civilRegistry/marriage/MatrimonialRegime.kt
@@ -0,0 +1,17 @@
+package newDocument.civilRegistry.marriage
+//(https://www.angloinfo.com/how-to/brazil/family/marriage-partnerships/marital-contracts)
+
+/**
+ * Representa o regime de bens do matrímonio
+ *
+ * @property PartialPropertyRuling          Comunhão Parcial
+ * @property CommunityPropertyRuling        Comunhão de Bens
+ * @property SeparatePropertyRuling         Separação de Bens
+ * @property FinalPartitionOfAcquisitions   Participação Final nos aquetos
+ */
+enum class MatrimonialRegime(val value:String) {
+    PartialPropertyRuling("Comunhão Parcial"),
+    CommunityPropertyRuling("Comunhão de Bens"),
+    SeparatePropertyRuling("Separação de Bens"),
+    FinalPartitionOfAcquisitions("Participação Final nos aquetos")
+}
---
+++ src/main/kotlin/newDocument/civilRegistry/marriage/Spouse.kt
@@ -0,0 +1,7 @@
+package newDocument.civilRegistry.marriage
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Spouse {
+}
