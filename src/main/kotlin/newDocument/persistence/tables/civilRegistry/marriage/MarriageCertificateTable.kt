64d6b4a1d17ee2b2ec7b3087975ffabcd4f99f2d
Arthur Marinheiro Vilela <av@westron.media>
2021-09-21 09:44:24 -0300

    Adicionadas tabelas para certidão de casamento

---
+++ src/main/kotlin/newDocument/persistence/tables/civilRegistry/marriage/MarriageCertificateTable.kt
@@ -0,0 +1,16 @@
+package newDocument.persistence.tables.civilRegistry.marriage
+
+import newDocument.civilRegistry.marriage.MatrimonialRegime
+import newDocument.persistence.tables.civilRegistry.CivilRegistryDocumentTable
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IdTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.`java-time`.date
+import java.util.*
+
+object MarriageCertificateTable:IdTable<UUID>("marriage_certificate") {
+    override val id: Column<EntityID<UUID>> = reference("id", CivilRegistryDocumentTable.id)
+
+    val dateOfRegistry = date("date_of_registry")
+    val matrimonialRegime = enumeration("matrimonial_regime", MatrimonialRegime::class)
+}
---
+++ src/main/kotlin/newDocument/persistence/tables/civilRegistry/marriage/SpouseTable.kt
@@ -0,0 +1,14 @@
+package newDocument.persistence.tables.civilRegistry.marriage
+
+import newDocument.persistence.tables.person.PhysicalPersonTable
+import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.`java-time`.date
+
+object SpouseTable:UUIDTable("spouse") {
+    val marriageCertificateId = reference("marriage_certificate_id", MarriageCertificateTable.id)
+    val personId = optReference("person_id", PhysicalPersonTable.id)
+    val singleName = varchar("single_name", 140)
+    val marriedName = varchar("married_name", 140)
+    val birthday = date("birthday")
+    val nationality = varchar("nationality", 60)
+}
