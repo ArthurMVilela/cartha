c2ec1b02f6e4dbf510ec1c347ed7030c3ff74efc
Arthur Marinheiro Vilela <av@westron.media>
2021-11-12 18:16:27 -0300

    Adicionado logging para outras funcionalidades

--- src/main/kotlin/authentication/logging/ActionType.kt
+++ src/main/kotlin/authentication/logging/ActionType.kt
@@ -13,6 +13,17 @@ enum class ActionType(val value: String) {
     AddNotary("criar cartório"),
     AddOfficialToNotary("adicionado funcionário ao cartório"),
     AddManagerToNotary("adicionado gerente ao cartório"),
-    CreateAccount("criar conta de usuário")
+    CreateAccount("criar conta de usuário"),
+    SeeBirthCertificate("ver certidão de nascimento"),
+    SeeBirthCertificates("ver certidões de nascimento"),
+    CreateBirthCertificates("criar certidão de nascimento"),
+    PrintBirthCertificates("Imprimir certidão de nascimento"),
+    SeeNodes("ver nós"),
+    SeeNode("ver nó"),
+    CreateNode("criar nó"),
+    SeeBlockchain("ver blockchain"),
+    SeeBlock("ver bloco"),
+    SeeDocumentTransactions("ver transações de documento"),
+    CheckDocumentValidity("checar validade de documento"),
 
 }
